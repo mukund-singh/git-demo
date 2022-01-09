bd4c5f257839428e5b462c35ec1bfc91fc9dbb70
mukund-singh <kumarbalmukund4u@gmail.com>
2022-01-09 16:35:21 +0530

    My secont commit

--- src/main/java/com/git/demo/GitDemoApplication.java
+++ src/main/java/com/git/demo/GitDemoApplication.java
@@ -2,6 +2,7 @@ package com.git.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GitDemoApplication {
 
-	@RequestMapping("/welcome")
+	@GetMapping("/welcome")
 	public String getWelcome(){
 		return "Welcome GIT-Demo project";
 	}
 
-	@RequestMapping("/welcome/{name}")
+	@GetMapping("/welcome/{name}")
 	public String getWelcomeName(@PathVariable String name){
 		return "Welcome "+name;
 	}
